8194647ae8c2b7046957978955cf4cebfea9489e
cougar91 <alpha.z91@mail.ru>
2019-01-17 17:50:37 +0600

    Добавьте файлы проекта.

---
+++ SendSMS/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace SendSMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+
+            user.EnterLogin();
+            user.EnterPassword();
+            user.EnterEmail();
+            user.EnterPhoneNumber();
+
+            string adoptedCode = VerificationAccount();
+
+            Console.WriteLine("We send your phone number verification code, please enter");
+            string verificationCode = Console.ReadLine();
+
+            if (verificationCode == adoptedCode)
+            {
+                Console.Clear();
+                Console.WriteLine("Congratulations! You succesfully passed registration");
+            }
+
+            Console.ReadLine();
+        }
+        static string VerificationAccount()
+        {
+            // Find your Account Sid and Token at twilio.com/console
+            const string accountSid = "AC9dc0d107f661a29db6e2db341af2beb4";
+            const string authToken = "d7d8170696c1de5bda1e96b4a9347018";
+
+            Random rnd = new Random();
+            string code = Convert.ToString(rnd.Next(100, 1000));
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                from: new Twilio.Types.PhoneNumber("+18432585652"),
+                body: code,
+                to: new Twilio.Types.PhoneNumber("+77719777518")
+            );
+            
+            Console.WriteLine(message.Sid);
+
+            return code;
+        }
+    }
+}
---
+++ SendSMS/User.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendSMS
+{
+    class User
+    {
+        private string login;
+        private string password;
+        private string email;
+        private string phoneNumber;
+
+        public string Login { get => login; set => login = value; }
+        public string Password { get => password; set => password = value; }
+        public string Email { get => email; set => email = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+
+        public void Show(int numb)
+        {
+            for (int i = numb; i <= numb; i++)
+            {
+                switch (i)
+                {
+                    case 1: Console.Write("Login: " + login); break;
+                    case 2: Console.Write("Password: " + password); break;
+                    case 3: Console.Write("Confirm Password: " + password); break;
+                    case 4: Console.Write("Email: " + email); break;
+                    case 5: Console.Write("Phone number: " + PhoneNumber); break;
+                }
+            }
+        }
+        public void EnterLogin()
+        {
+            const int FIELD_NUMBER = 1;
+            char[] prohibitedSymbols = { '/', '\\', ':', ';', '[', ']', '|', '=', ',', '+', '*', '?', '<', '>' };
+
+            Show(FIELD_NUMBER);
+            login = Console.ReadLine();
+
+            //if (string.IsNullOrWhiteSpace(login))
+            //{
+            //    Console.WriteLine("Логин содержит недопустимые символы");
+            //}
+
+            for (int i = 0; i < login.Length; i++)
+            {
+                for (int j = 0; j < prohibitedSymbols.Length; j++)
+                {
+                    if (login[i] == prohibitedSymbols[j])
+                    {
+                        Console.WriteLine("Логин содержит недопустимые символы");
+                    }
+
+                }
+            }
+        }
+        public void EnterPassword()
+        {
+            const int FIELD_NUMBER_2 = 2, FIELD_NUMBER_3 = 3;
+            char[] prohibitedSymbols = { '/', '\\', ':', ';', '[', ']', '|', '=', ',', '+', '*', '?', '<', '>' };
+
+            Show(FIELD_NUMBER_2);
+            password = Console.ReadLine();
+            for (int i = 0; i < password.Length; i++)
+            {
+                for (int j = 0; j < prohibitedSymbols.Length; j++)
+                {
+                    if (password[i] == prohibitedSymbols[j])
+                    {
+                        Console.WriteLine("Пароль содержит недопустимые символы");
+                    }
+
+                }
+            }
+
+            Show(FIELD_NUMBER_3);
+            string confirmPassword = Console.ReadLine();
+
+            if(password != confirmPassword)
+            {
+                Console.WriteLine("Пароли не совпадают");
+            }
+        }
+        public void EnterEmail()
+        {
+            const int FIELD_NUMBER_4 = 4;
+            Show(FIELD_NUMBER_4);
+            email = Console.ReadLine();
+        }
+        public void EnterPhoneNumber()
+        {
+            const int FIELD_NUMBER_5 = 5;
+            Show(FIELD_NUMBER_5);
+            phoneNumber = (Console.ReadLine());
+        }
+    }
+}
