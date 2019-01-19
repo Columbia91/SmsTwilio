d87722bc74cb07186d7edd919c9bf9b7af449886
ADILET <alpha.z91@mail.ru>
2019-01-19 21:17:54 +0600

    Применение во всех методах регулярные выражения

--- SendSMS/Program.cs
+++ SendSMS/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 
@@ -14,26 +15,187 @@ namespace SendSMS
         {
             User user = new User();
 
-            user.EnterLogin();
-            user.EnterPassword();
-            user.ConfirmPassword();
-            user.EnterEmail();
-            user.EnterPhoneNumber();
+            EnterLogin(user);
+            EnterPassword(user);
+            ConfirmPassword(user);
+            EnterEmail(user);
+            EnterPhoneNumber(user);
+            
+            string adoptedCode = VerificationAccount(user);
 
-            string adoptedCode = VerificationAccount();
-
-            Console.WriteLine("We sent your phone number verification code, please enter");
+            Console.Write("\nМы отправили на указанный Вами номер код верификации, введите его ниже\nКод подтверждения: ");
             string verificationCode = Console.ReadLine();
 
             if (verificationCode == adoptedCode)
             {
                 Console.Clear();
-                Console.WriteLine("Congratulations! You succesfully passed registration");
+                Console.WriteLine("Поздравляем! Вы успешно прошли регистрацию");
             }
 
             Console.ReadLine();
         }
-        static string VerificationAccount()
+
+        #region Вывод на консоль
+        static void Show(User user, int numb)
+        {
+            for (int i = 0; i < numb; i++)
+            {
+                switch (i + 1)
+                {
+                    case 1: Console.Write("Login: " + user.Login); break;
+                    case 2: Console.Write("\nPassword: " + user.Password); break;
+                    case 3: Console.Write("\nConfirm Password: " + user.PasswordCopy); break;
+                    case 4: Console.Write("\nEmail: " + user.Email); break;
+                    case 5: Console.Write("\nPhone number: " + user.PhoneNumber); break;
+                }
+            }
+        }
+        #endregion
+
+        #region Ввод логина
+        static void EnterLogin(User user)
+        {
+            Console.Clear();
+            const int FIELD_NUMBER = 1;
+
+            Show(user, FIELD_NUMBER);
+            user.Login = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(user.Login))
+            {
+                Console.WriteLine("Логин содержит недопустимые символы, нажмите Enter чтобы ввести заново...");
+                user.Login = "";
+                Console.ReadKey();
+                EnterLogin(user);
+            }
+
+            Regex regex = new Regex(@"\W|[А-Яа-я]+");
+            MatchCollection matches = regex.Matches(user.Login);
+            if (matches.Count > 0)
+            {
+                Console.WriteLine("Логин содержит недопустимые символы, нажмите Enter чтобы ввести заново...");
+                user.Login = "";
+                Console.ReadKey();
+                EnterLogin(user);
+            }
+        }
+        #endregion
+
+        #region Ввод пароля
+        static void EnterPassword(User user)
+        {
+            Console.Clear();
+            const int FIELD_NUMBER_2 = 2, MIN_PASSWORD_LENGTH = 6;
+            bool isThereNumber = false, isThereUppercaseLetter = false, isThereLowercaseLetter = false;
+
+            Show(user, FIELD_NUMBER_2);
+            user.Password = Console.ReadLine();
+
+            Regex regex = new Regex(@"\W|[А-Яа-я]+");
+            MatchCollection matches = regex.Matches(user.Password);
+
+            for (int i = 0; i < user.Password.Length; i++)
+            {
+                if (user.Password[i] >= 'A' && user.Password[i] <= 'Z') isThereUppercaseLetter = true;
+                else if (user.Password[i] >= 'a' && user.Password[i] <= 'z') isThereLowercaseLetter = true;
+                else if (user.Password[i] >= '0' && user.Password[i] <= '9') isThereNumber = true;
+            }
+            if (!isThereNumber || !isThereUppercaseLetter || !isThereLowercaseLetter)
+            {
+                Console.WriteLine("Пароль должен содержать цифровой символ, и буквы верхнего, нижнего регистра");
+                user.Password = "";
+                Console.ReadKey();
+                EnterPassword(user);
+            }
+
+            while (true)
+            {
+                if (string.IsNullOrWhiteSpace(user.Password))
+                {
+                    Console.WriteLine("Пароль содержит недопустимые символы, нажмите Enter чтобы ввести заново...");
+                }
+                else if (user.Password.Length < MIN_PASSWORD_LENGTH)
+                {
+                    Console.WriteLine("Длина пароля должна быть больше {0} символов, нажмите Enter чтобы ввести заново...", MIN_PASSWORD_LENGTH);
+                }
+                else if (matches.Count > 0)
+                {
+                    Console.WriteLine("Пароль содержит недопустимые символы, нажмите Enter чтобы ввести заново...");
+                }
+                else
+                {
+                    break;
+                }
+                user.Password = "";
+                Console.ReadKey();
+                EnterPassword(user);
+            }
+        }
+        #endregion
+
+        #region Подтверждение пароля
+        static void ConfirmPassword(User user)
+        {
+            Console.Clear();
+            const int FIELD_NUMBER_3 = 3;
+            Show(user, FIELD_NUMBER_3);
+            user.PasswordCopy = Console.ReadLine();
+
+            if (user.Password != user.PasswordCopy)
+            {
+                Console.WriteLine("Пароли не совпадают, нажмите Enter чтобы ввести заново...");
+                user.PasswordCopy = "";
+                Console.ReadKey();
+                ConfirmPassword(user);
+            }
+        }
+        #endregion
+
+        #region Ввод почты
+        static void EnterEmail(User user)
+        {
+            Console.Clear();
+            const int FIELD_NUMBER_4 = 4;
+            Show(user, FIELD_NUMBER_4);
+
+            user.Email = Console.ReadLine();
+
+            string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
+
+            if (!(Regex.IsMatch(user.Email, pattern, RegexOptions.IgnoreCase)))
+            {
+                Console.WriteLine("Некорректный email, нажмите Enter чтобы ввести заново...");
+                user.Email = "";
+                Console.ReadKey();
+                EnterEmail(user);
+            }
+        }
+        #endregion
+
+        #region Ввод номера
+        static void EnterPhoneNumber(User user)
+        {
+            Console.Clear();
+            const int FIELD_NUMBER_5 = 5;
+
+            Show(user, FIELD_NUMBER_5);
+            user.PhoneNumber= Console.ReadLine();
+
+            string pattern = @"^\+?[7]\d{10}$";
+
+            if (!(Regex.IsMatch(user.PhoneNumber, pattern, RegexOptions.IgnoreCase)))
+            {
+                Console.WriteLine("Телефонный номер содержит недопустимые символы или введен не корректно, нажмите Enter чтобы ввести заново...");
+                user.PhoneNumber = "";
+                Console.ReadKey();
+                EnterPhoneNumber(user);
+            }
+        }
+        #endregion
+
+        #region Верификация аккаунта
+        static string VerificationAccount(User user)
         {
             // Find your Account Sid and Token at twilio.com/console
             const string accountSid = "AC9dc0d107f661a29db6e2db341af2beb4";
@@ -47,12 +209,13 @@ namespace SendSMS
             var message = MessageResource.Create(
                 from: new Twilio.Types.PhoneNumber("+18432585652"),
                 body: code,
-                to: new Twilio.Types.PhoneNumber("+77719777518")
+                to: new Twilio.Types.PhoneNumber("+77719777518") // user.PhoneNumber
             );
             
-            Console.WriteLine(message.Sid);
+            // Console.WriteLine(message.Sid);
 
             return code;
         }
+        #endregion
     }
 }
--- SendSMS/User.cs
+++ SendSMS/User.cs
@@ -10,134 +10,15 @@ namespace SendSMS
     {
         private string login;
         private string password;
+        private string passwordCopy;
         private string email;
         private string phoneNumber;
 
         public string Login { get => login; set => login = value; }
         public string Password { get => password; set => password = value; }
+        public string PasswordCopy { get => passwordCopy; set => passwordCopy = value; }
         public string Email { get => email; set => email = value; }
         public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
-
-        public void Show(int numb)
-        {
-            switch (numb)
-            {
-                case 1: Console.Write("Login: "); break;
-                case 2: Console.Write("Password: "); break;
-                case 3: Console.Write("Confirm Password: "); break;
-                case 4: Console.Write("Email: "); break;
-                case 5: Console.Write("Phone number: "); break;
-            }
-        }
-
-        #region Ввод логина
-        public void EnterLogin()
-        {
-            const int FIELD_NUMBER = 1;
-            char[] prohibitedSymbols = { ' ', '/', '\\', ':', ';', '[', ']', '|', '=', ',', '+', '*', '?', '<', '>' };
-
-            Show(FIELD_NUMBER);
-            login = Console.ReadLine();
-
-            if (string.IsNullOrWhiteSpace(login))
-            {
-                Console.WriteLine("Логин содержит недопустимые символы, нажмите Enter чтобы ввести заново...");
-                Console.ReadKey();
-                Console.Clear();
-                EnterLogin();
-            }
-
-            for (int i = 0; i < login.Length; i++)
-            {
-                for (int j = 0; j < prohibitedSymbols.Length; j++)
-                {
-                    if (login[i] == prohibitedSymbols[j])
-                    {
-                        Console.WriteLine("Логин содержит недопустимые символы, нажмите Enter чтобы ввести заново...");
-                        Console.ReadKey();
-                        Console.Clear();
-                        EnterLogin();
-                    }
-                    else if((login[i] >= 'А' && login[i] <= 'Я') || (login[i] >= 'а' && login[i] <= 'я'))
-                    {
-                        Console.WriteLine("Логин содержит недопустимые символы, нажмите Enter чтобы ввести заново...");
-                        Console.ReadKey();
-                        Console.Clear();
-                        EnterLogin();
-                    }
-                }
-            }
-        }
-        #endregion
-        #region Ввод пароля
-        public void EnterPassword()
-        {
-            const int FIELD_NUMBER_2 = 2;
-            char[] prohibitedSymbols = { ' ', '/', '\\', ':', ';', '[', ']', '|', '=', ',', '+', '*', '?', '<', '>' };
-
-            Show(FIELD_NUMBER_2);
-            password = Console.ReadLine();
-
-            if (string.IsNullOrWhiteSpace(password))
-            {
-                Console.WriteLine("Пароль содержит недопустимые символы, нажмите Enter чтобы ввести заново...");
-                Console.ReadKey();
-                Console.Clear();
-                EnterPassword();
-            }
-
-            for (int i = 0; i < password.Length; i++)
-            {
-                for (int j = 0; j < prohibitedSymbols.Length; j++)
-                {
-                    if (password[i] == prohibitedSymbols[j])
-                    {
-                        Console.WriteLine("Пароль содержит недопустимые символы, нажмите Enter чтобы ввести заново...");
-                        Console.ReadKey();
-                        Console.Clear();
-                        EnterPassword();
-                    }
-                    else if ((password[i] >= 'А' && password[i] <= 'Я') || (password[i] >= 'а' && password[i] <= 'я'))
-                    {
-                        Console.WriteLine("Пароль содержит недопустимые символы, нажмите Enter чтобы ввести заново...");
-                        Console.ReadKey();
-                        Console.Clear();
-                        EnterPassword();
-                    }
-                }
-            }
-        }
-        #endregion
-
-        #region Подтверждение пароля
-        public void ConfirmPassword()
-        {
-            const int FIELD_NUMBER_3 = 3;
-            Show(FIELD_NUMBER_3);
-            string confirmPassword = Console.ReadLine();
-
-            if (password != confirmPassword)
-            {
-                Console.WriteLine("Пароли не совпадают, нажмите Enter чтобы ввести заново...");
-                Console.ReadKey();
-                Console.Clear();
-                ConfirmPassword();
-            }
-        }
-        #endregion
-
-        public void EnterEmail()
-        {
-            const int FIELD_NUMBER_4 = 4;
-            Show(FIELD_NUMBER_4);
-            email = Console.ReadLine();
-
-        }
-        public void EnterPhoneNumber()
-        {
-            const int FIELD_NUMBER_5 = 5;
-            Show(FIELD_NUMBER_5);
-            phoneNumber = (Console.ReadLine());
-        }
+        
     }
 }
